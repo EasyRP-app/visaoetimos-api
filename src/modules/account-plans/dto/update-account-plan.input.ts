6cc892bac9b692e18b28f51ecf2cccc997d9f8b1
Ulguim <williamulguim2014@gmail.com>
2022-11-26 17:19:18 -0300

    :lock: ISSUE-17 Fix update entity

--- src/modules/account-plans/dto/update-account-plan.input.ts
+++ src/modules/account-plans/dto/update-account-plan.input.ts
@@ -5,6 +5,6 @@ import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 export class UpdateAccountPlanInput extends PartialType(
   CreateAccountPlanInput,
 ) {
-  @Field(() => ID)
-  id: string;
+  @Field(() => ID, { nullable: true })
+  id?: string;
 }
--- src/modules/finances/dto/update-finance.input.ts
+++ src/modules/finances/dto/update-finance.input.ts
@@ -3,6 +3,6 @@ import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateFinanceInput extends PartialType(CreateFinanceInput) {
-  @Field(() => ID)
-  id: string;
+  @Field(() => ID, { nullable: true })
+  id?: string;
 }
