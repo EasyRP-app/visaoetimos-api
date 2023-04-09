a369c9a59e4c278bcdb5d4923814638d05dadaff
william.ulguim <william.ulguim.ext@wundermanthompson.com>
2023-04-09 07:09:30 -0300

    :art: Improves scalability

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,14 @@ import { AppService } from './app.service';
 import { AccountPlansModule } from './modules/account-plans/account-plans.module';
 import { AuthModule } from './modules/auth/auth.module';
 // import { AuthModule } from './modules/auth/auth.module';
+import { AppDataSource } from './db/data-source';
 import { FinancesModule } from './modules/finances/finances.module';
 import { SuppliersAndCustomersModule } from './modules/suppliers-and-customers/suppliers-and-customers.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot(
       // type: 'postgres',
       // host: 'localhost',
       // port: 5432,
@@ -25,16 +26,19 @@ import { UsersModule } from './modules/users/users.module';
       // entities: ['**/modules/**/entities/*.js'],
       // synchronize: true,
       // logging: true,
-      type: 'postgres',
-      host: process.env.TYPEORM_HOST,
-      port: +process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [process.env.TYPEORM_ENTITIES],
-      synchronize: !!process.env.TYPEORM_SYNCHRONIZE,
-      logging: !!process.env.TYPEORM_LOGGING,
-    }),
+      // type: 'postgres',
+      // host: process.env.TYPEORM_HOST,
+      // port: +process.env.TYPEORM_PORT,
+      // username: process.env.TYPEORM_USERNAME,
+      // password: process.env.TYPEORM_PASSWORD,
+      // database: process.env.TYPEORM_DATABASE,
+      // entities: [process.env.TYPEORM_ENTITIES],
+      // synchronize: !!process.env.TYPEORM_SYNCHRONIZE,
+      // logging: !!process.env.TYPEORM_LOGGING,
+      // migrations: ['dist/**/migration/*.js'],
+      // subscribers: ['dist/**/**.subscriber{.ts,.js}'],
+      AppDataSource,
+    ),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
